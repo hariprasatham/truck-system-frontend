eb3644bfd1c21d9995bc838c6a3fc3517b625aff
Hariprasath A <hariprasatham@gmail.com>
2025-11-29 20:43:30 +0530

    conflict resolved in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,8 @@ import PreEmploymentApplicationList from "./pages/PreEmploymentApplicationList";
 
 import ErrorBoundary from "./components/ErrorBoundary";
 import Brand from "./pages/Brand";
-<<<<<<< Updated upstream
 import FuelUnit from "./pages/FuelUnit";
-=======
 import IncidentDetails from "./pages/IncidentDetails";
->>>>>>> Stashed changes
 
 const App = () => {
   window.bootstrap = bootstrap; // ✅ Make Bootstrap globally available
@@ -46,14 +43,6 @@ const App = () => {
     <>
       {/* <GlobalLoader /> */}
       <ErrorBoundary>
-<<<<<<< Updated upstream
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* Public route that should not be accessible when logged in */}
-            <Route element={<PublicRoute />}>
-              <Route path="/login" element={<Login />} />
-=======
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -83,62 +72,12 @@ const App = () => {
               <Route path="/brand" element={<Brand />} />
               <Route path="/incident-management" element={<IncidentManagement />} />
               <Route path="/incident-management/:incidentId" element={<IncidentDetails />} />
+              <Route path="/fuelunit" element={<FuelUnit />} />
               
               {/* <Route path="/drivers" element={<DriverManagement />} /> */}
               {/* <Route path="/profile" element={<Profile />} /> */}
->>>>>>> Stashed changes
             </Route>
-
-            {/* Protected Routes */}
-            <Route element={<ProtectedRoute />}>
-              <Route element={<Layout />}>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/companies" element={<Companies />} />
-                <Route
-                  path="/companies/:companyId/user-management"
-                  element={<UserManagement />}
-                />
-                <Route
-                  path="/companies/:companyId/user-management/:userId/drivers"
-                  element={<DriverList />}
-                />
-                <Route
-                  path="/companies/:companyId/user-management/:userId/drivers/:driverId"
-                  element={<DriverDetails />}
-                />
-                <Route
-                  path="/companies/:companyId/user-management/:userId/trucks"
-                  element={<TruckManagement />}
-                />
-
-                <Route
-                  path="/driver-management"
-                  element={<DriverManagement />}
-                />
-                <Route path="/menu-management" element={<MenuManagement />} />
-                <Route path="/truck-management" element={<TruckManagement />} />
-                <Route
-                  path="/fuel-invoice-management"
-                  element={<FuelInvoiceManagement />}
-                />
-                <Route
-                  path="/view-fuel-data/:fuelId"
-                  element={<ViewFuelData />}
-                />
-                <Route
-                  path="/pre-employment-application/form"
-                  element={<PreEmploymentCheck />}
-                />
-                <Route
-                  path="/pre-employment-application"
-                  element={<PreEmploymentApplicationList />}
-                />
-                <Route path="/brand" element={<Brand />} />
-                <Route path="/fuelunit" element={<FuelUnit />} />
-                {/* <Route path="/drivers" element={<DriverManagement />} /> */}
-                {/* <Route path="/profile" element={<Profile />} /> */}
-              </Route>
-            </Route>
+          </Route>
 
             {/* Catch-all for unmatched routes */}
             <Route path="*" element={<NotFound />} />
